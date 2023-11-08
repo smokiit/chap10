56c8e23b9c8273fb0dd3696698e77a0b50bb805b
smokiit <smo.nicek08@yahoo.com>
2023-11-08 15:24:55 +0900

    ex6 - ex7 done

---
+++ app/src/main/java/com/example/chap10/ex6.java
@@ -0,0 +1,42 @@
+package com.example.chap10;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.Button;
+
+public class ex6 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ex6);
+        final RatingBar rating1, rating2, rating3;
+        Button btnInc, btnDec;
+
+        rating1 = (RatingBar) findViewById(R.id.ratingBar1);
+        rating2 = (RatingBar) findViewById(R.id.ratingBar2);
+        rating3 = (RatingBar) findViewById(R.id.ratingBar3);
+        btnInc = (Button) findViewById(R.id.btnInc);
+        btnDec = (Button) findViewById(R.id.btnDec);
+
+        btnInc.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                rating1.setRating(rating1.getRating()+rating1.getStepSize());
+                rating2.setRating(rating2.getRating()+rating2.getStepSize());
+                rating3.setRating(rating3.getRating()+rating3.getStepSize());
+            }
+        });
+
+        btnDec.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                rating1.setRating(rating1.getRating()-rating1.getStepSize());
+                rating2.setRating(rating2.getRating()-rating2.getStepSize());
+                rating3.setRating(rating3.getRating()-rating3.getStepSize());
+            }
+        });
+    }
+}
